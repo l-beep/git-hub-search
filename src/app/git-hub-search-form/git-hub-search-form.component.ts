5ee94c85a7381cf9ce20cf18003620fe77f02ead
LOvine <oduor.lovine@gmail.com>
2020-06-21 21:56:38 +0300

    import the repository component to the git form component

--- src/app/git-hub-search-form/git-hub-search-form.component.ts
+++ src/app/git-hub-search-form/git-hub-search-form.component.ts
@@ -2,24 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import {Repository} from 'src/app/repository';
 import {GitsearcherService} from 'src/app/Gitsearch/gitsearcher.service'
 import {User} from 'src/app/user';
-import { git-hub-searchComponent } from 'src/app/git-hub-search/git-hub-search.component';
+import { GitsearchComponent } from 'src/app/gitsearch/gitsearch.component';
 import { AppComponent } from 'src/app/app.component';
 import {FormsModule} from '@angular/forms';
 
 
-
-
-
 @Component({
-  selector: 'app-git-hub-search-form',
-  templateUrl: './git-hub-search-form.component.html',
-  styleUrls: ['./git-hub-search-form.component.css']
+  selector: 'app-gitsearch-form',
+  templateUrl: './gitsearch-form.component.html',
+  styleUrls: ['./gitsearch-form.component.css']
 })
-export class GitHubSearchFormComponent implements OnInit {
+export class GitsearchFormComponent implements OnInit {
+  details: any ;
+  repos:any;
+  username: string;
 
-  constructor() { }
+  constructor(private gitsearcherService: GitsearcherService) { }
 
   ngOnInit() {
   }
-
+  findUser(){
+    this.gitsearcherService.updateProfile(this.username);
+    this.gitsearcherService.getProfileInfo(this.username).subscribe(details => {
+      console.log(details);
+      this.details = details;
+    });
+    this.gitsearcherService.getRepoInfo().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+  })
 }
+}
+ 
--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {Repository} from 'src/app/repository';
+import {User} from 'src/app/user';
+import { GitsearcherService } from 'src/app/Gitsearch/gitsearcher.service'
+import { git-hub-search-formComponent } from 'src/app/git-hub-search-form/git-hub-search-form.component';
+import { GitsearchComponent } from 'src/app/gitsearch/git-search.component';
+import { AppComponent } from 'src/app/app.component';
+import {FormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-repositories',
@@ -6,10 +14,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
+  details: any;
+  repos: any;
+  username:string;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private gitsearcherService: GitsearcherService) { }
+
+  ngOnInit() {
   }
+findUser(){
+
+  this.gitsearcherService.getRepoInfo().subscribe(repos => {
+    console.log(repos);
+    this.repos = repos;
+})
 
 }
+}
+
