1ea2a4f5038e383e432ba687b99ebf8b96409e6d
LOvine <oduor.lovine@gmail.com>
2020-06-21 21:19:53 +0300

    added an ngfor identifier in the repo content identifier

--- src/app/git-hub-search-form/git-hub-search-form.component.ts
+++ src/app/git-hub-search-form/git-hub-search-form.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {Repository} from 'src/app/repository';
+import {GitsearcherService} from 'src/app/Gitsearch/gitsearcher.service'
+import {User} from 'src/app/user';
+import { git-hub-searchComponent } from 'src/app/git-hub-search/git-hub-search.component';
+import { AppComponent } from 'src/app/app.component';
+import {FormsModule} from '@angular/forms';
+
+
+
+
 
 @Component({
   selector: 'app-git-hub-search-form',
@@ -9,7 +19,7 @@ export class GitHubSearchFormComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
